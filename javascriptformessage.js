0d6f6a239e189ad58d83a31f3776faba150d916d
PoornaBharanikumar <poornabharanikumar11@gmail.com>
2023-02-12 00:42:26 -0600

    Update

---
+++ javascriptformessage.js
@@ -0,0 +1,16 @@
+let sleep = "go to sleep";
+let fog =  "be safe it's foggy";
+let wind = "it's windy";
+let rain = "ice on the road";
+
+if ()
+{
+    console.log(sleep)
+}
+
+else 
+{
+    console
+}
+
+
